a5937b7629d940d52687de17c92409d2c58007ac
MariamNsubuga <30923697+MariamNsubuga@users.noreply.github.com>
2018-09-17 19:16:46 +0300

    modify challenge 2

--- challenge2.py
+++ challenge2.py
@@ -3,13 +3,3 @@ nums={}
 for num in range (1,16):
     nums [num] = num ** 2
 print(nums)
-#else:
- #print("enter a number between 1 and 15")
-#for x in range(1,16):
- #   d[x]=x**2
-#print(d)  
- #test = { input(): input() }
-#bob
-#password
-#>>> test
-#{'bob': 'password'}
